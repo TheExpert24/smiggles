5be232a039be16ff2e51d2f24d2090e960b05c13
TheExpert24 <express.vanukuri@gmail.com>
2026-04-27 16:46:53 -0700

    fixed the divide by 0 problem.

--- myos/calculator.c
+++ myos/calculator.c
@@ -3,10 +3,10 @@
 // --- Calculator Parser ---
 
 // Forward declarations
-static int parse_expr(const char **s);
-static int parse_term(const char **s);
-static int parse_power(const char **s);
-static int parse_factor(const char **s);
+static int parse_expr(const char **s, char* video, int* cursor);
+static int parse_term(const char **s, char* video, int* cursor);
+static int parse_power(const char **s, char* video, int* cursor);
+static int parse_factor(const char **s, char* video, int* cursor);
 static int parse_number(const char **s);
 static void skip_spaces(const char **s);
 
@@ -29,12 +29,12 @@ static int parse_number(const char **s) {
     return neg ? -val : val;
 }
 
-static int parse_factor(const char **s) {
+static int parse_factor(const char **s, char* video, int* cursor) {
     skip_spaces(s);
     int val;
     if (**s == '(') {
         (*s)++;
-        val = parse_expr(s);
+        val = parse_expr(s, video, cursor);
         skip_spaces(s);
         if (**s == ')') (*s)++;
         return val;
@@ -43,12 +43,12 @@ static int parse_factor(const char **s) {
     }
 }
 
-static int parse_power(const char **s) {
-    int base = parse_factor(s);
+static int parse_power(const char **s, char* video, int* cursor) {
+    int base = parse_factor(s, video, cursor);
     skip_spaces(s);
     while (**s == '^') {
         (*s)++;
-        int exp = parse_power(s); // right-associative
+        int exp = parse_power(s, video, cursor); // right-associative
         int result = 1;
         for (int i = 0; i < exp; i++) result *= base;
         base = result;
@@ -57,27 +57,36 @@ static int parse_power(const char **s) {
     return base;
 }
 
-static int parse_term(const char **s) {
-    int val = parse_power(s);
+static int parse_term(const char **s, char* video, int* cursor) {
+    int val = parse_power(s, video, cursor);
     skip_spaces(s);
     while (**s == '*' || **s == '/') {
         char op = **s;
         (*s)++;
-        int rhs = parse_power(s);
-        if (op == '*') val *= rhs;
-        else if (op == '/') val /= rhs;
+        int rhs = parse_power(s, video, cursor);
+        if (op == '*') {
+            val *= rhs;
+        } else if (op == '/') {
+            if (rhs == 0) {
+                // Print error message in red and return a special error code
+                print_string("stop dividing by 0 idiot", 25, video, cursor, COLOR_LIGHT_RED);
+                return 0x7FFFFFFF; // special error code for div by zero
+            } else {
+                val /= rhs;
+            }
+        }
         skip_spaces(s);
     }
     return val;
 }
 
-static int parse_expr(const char **s) {
-    int val = parse_term(s);
+static int parse_expr(const char **s, char* video, int* cursor) {
+    int val = parse_term(s, video, cursor);
     skip_spaces(s);
     while (**s == '+' || **s == '-') {
         char op = **s;
         (*s)++;
-        int rhs = parse_term(s);
+        int rhs = parse_term(s, video, cursor);
         if (op == '+') val += rhs;
         else if (op == '-') val -= rhs;
         skip_spaces(s);
@@ -94,7 +103,11 @@ int is_math_expr(const char* s) {
 
 void handle_calc_command(const char* expr, char* video, int* cursor) {
     const char* s = expr;
-    int result = parse_expr(&s);
+    int result = parse_expr(&s, video, cursor);
+    if (result == 0x7FFFFFFF) {
+        // Division by zero error already printed, do not print anything else
+        return;
+    }
     char buf[32];
     int n = 0, r = result;
     int neg = 0;
